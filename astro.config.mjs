860105702074de2b7bf82acc748966d3c03e615f
atushiOS <atushios.macer@gmail.com>
2026-01-07 07:14:41 +0900

    🔧Google アナリティクスの設定を追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
-    },
+  vite: {
+      plugins: [tailwindcss()],
+  },
+
+  integrations: [partytown()],
 });
